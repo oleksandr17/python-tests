737d2c3c27357d31c1a2a765d040032b81b57bc5
Oleksandr <oleksandr@elements.nl>
2018-10-15 09:30:29 +0200

    Add more examples

--- 3/test.py
+++ 3/test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, Mock, create_autospec, patch
+from unittest.mock import MagicMock, Mock, call, create_autospec, patch
 
 import main
 
@@ -34,15 +34,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(mock(), 100)
         self.assertEqual(mock(), 200)
 
-    def test_patch_1(self):
-        with patch.object(main.Production, 'deploy', return_value=False):
-            thing = main.Production()
-            self.assertFalse(thing.deploy())
-
-        # Outside patch scope
-        thing = main.Production()
-        self.assertTrue(thing.deploy())
-
     def test_magic_methods(self):
         # Magic mock
         magic_mock = MagicMock()
@@ -71,3 +62,75 @@ class TestCase(unittest.TestCase):
         
         production_mock.deploy()
         production_mock.deploy.assert_called()
+
+    def test_assert_called_with(self):
+        mock = Mock()
+        mock.method(1, 2, 3, test='wow')
+        mock.method.assert_called_with(1, 2, 3, test='wow')
+
+    def test_assert_has_calls(self):
+        mock = Mock()
+        mock(1)
+        mock(2)
+        mock(3)
+        mock(4)
+
+        # Order matters
+        calls = [call(3), call(2), call(1)]
+        mock.assert_has_calls(calls, any_order=True)
+
+        # Order doesn't matter
+        with self.assertRaises(AssertionError):
+            calls = [call(3), call(2), call(1)]
+            mock.assert_has_calls(calls)
+
+    def test_call_count(self):
+        mock = Mock(return_value=None)
+        self.assertEqual(mock.call_count, 0)
+
+        mock()
+        mock()
+        self.assertEqual(mock.call_count, 2)
+
+    def test_call_args(self):
+        mock = MagicMock()
+
+        mock()
+        self.assertEqual(mock.call_args, call())
+
+        mock(1, 2, 3)
+        self.assertEqual(mock.call_args, call(1 ,2, 3))
+
+    def test_method_calls(self):
+        mock = Mock()
+        mock.method()
+        mock.attribute.method()
+        self.assertEqual(mock.method_calls, [call.method(), call.attribute.method])
+
+    def test_patch_1(self):
+        with patch.object(main.Production, 'deploy', return_value=False):
+            thing = main.Production()
+            self.assertFalse(thing.deploy())
+
+        # Outside patch scope
+        thing = main.Production()
+        self.assertTrue(thing.deploy())
+
+    def test_patch_2(self):
+            ProductionOriginalClass = main.Production
+
+            # Mock class
+            patcher = patch('main.Production', spec=True)
+            ProductionMockClass = patcher.start()
+            mock = ProductionMockClass()
+
+            # Verify class mocking
+            self.assertEqual(main.Production, ProductionMockClass)
+            assert isinstance(mock, ProductionOriginalClass)
+
+            # Mock method of instance
+            mock.deploy.return_value = False
+            self.assertFalse(mock.deploy())
+
+            # Stop mocking
+            patcher.stop()
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Purpose of this repo is to practice unit tests in Python.
 The following sources were used:
-0) https://docs.python.org/3/library/unittest.mock.html
-1) https://semaphoreci.com/community/tutorials/getting-started-with-mocking-in-python
-2) https://www.toptal.com/python/an-introduction-to-mocking-in-python
-3) http://blog.codepipes.com/testing/software-testing-antipatterns.html
+- https://semaphoreci.com/community/tutorials/getting-started-with-mocking-in-python
+- https://www.toptal.com/python/an-introduction-to-mocking-in-python
+- https://docs.python.org/3/library/unittest.mock.html
+- http://blog.codepipes.com/testing/software-testing-antipatterns.html
